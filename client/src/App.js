7f485e6372f6da032f34d2a4926b23540b86e611
Martina John <martina07j@gmail.com>
2022-10-04 22:51:18 +0530

    Login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import { BrowserRouter, Routes,Route } from "react-router-dom"
+// // import Login from "./Pages/Login";
+// import SignInOutContainer from './Containers/container';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <BrowserRouter>
+//     <div className="container">
+//     {/* <Navbar /> */}
+//       <br />
+//       <Routes>
+//         <Route path = "/login" exact element = {<SignInOutContainer />} /> 
+//       </Routes>
+//     </div>
+      
+//     </BrowserRouter>
+      
+//   );
+// }
+
+// export default App;
+import React, { Component } from 'react';
+import Login from './Pages/Login' ;
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<Login />
+			</div>
+		);
+	}
 }
 
 export default App;
