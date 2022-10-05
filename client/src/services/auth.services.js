6cb72b2442963fd402c029d7387b3e6f3f0230f5
Suzan Dsouza <suzandsouza1505@gmail.com>
2022-10-05 12:26:53 +0530

    admin_list

---
+++ client/src/services/auth.services.js
@@ -0,0 +1,19 @@
+// require('dotenv').config();
+// import { ATLAS_URI } from "";
+// import axios from "axios";
+ 
+
+// export const SignUp = async (body) => {
+//   const response = await axios.post(`${ATLAS_URI}/users/register`, body);
+//   return response.data;
+// };
+
+// export const SignIn = async (body) => {
+//   const response = await axios.post(`${ATLAS_URL}/users/login`, body);
+//   return response.data;
+// };
+
+// export const SignOut = async (body) => {
+//   const response = await axios.post(`${ATLAS_URL}/users/logout`, body);
+//   return response.data;
+// };
--- server/models/organizer.js
+++ server/models/organizer.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const exerciseSchema = new Schema({
+
     username: {type:String, require: true},
+    eventname: {type:String, require: true},
     description:{type: String, require: true},
     duration:{type: Number, require: true},
     date:{type: Date, require: true},
